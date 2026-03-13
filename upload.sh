088ef9bcda01adfad851479c3663077eb99c0909
paiskomom <midooneteam@gmail.com>
2026-03-13 13:34:20 +0700

    Push

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/whyred/qassa*.zip ]; then
-      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/whyred/qassa*.zip komom:qassa -P
-      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/whyred/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/whyred/RROS*.zip ]; then
+      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls RROS*.zip)"
+      rclone copy ~/rom/out/target/product/whyred/RROS*.zip komom:komom -P
+      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/whyred/ && ls RROS*.zip) Uploaded Successfully!"
 fi
