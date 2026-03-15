5416608b8049d96f085649d0468a2b625d79c76b
paiskomom <midooneteam@gmail.com>
2026-03-15 19:49:39 +0700

    Push

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/whyred/conquer*.zip ]; then
-      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/whyred/ && ls conquer*.zip)"
-      rclone copy ~/rom/out/target/product/whyred/conquer*.zip komom:conquer -P
-      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/whyred/ && ls conquer*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/surya/Nusantara*.zip ]; then
+      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/surya/ && ls Nusantara*.zip)"
+      rclone copy ~/rom/out/target/product/surya/Nusantara*.zip komom:nusantara -P
+      curl -s https://api.telegram.org/bot$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/surya/ && ls Nusantara*.zip) Uploaded Successfully!"
 fi
